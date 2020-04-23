7b7923821390c8a0275bb9f7ba2e75d9ba352468
Sandman <antonio.lima16@fatec.sp.gov.br>
2020-04-23 17:23:17 -0300

    Initial state of the Client created succesfully. I also made the changes on the UI setting up the MUI framework and using the react-router-dom to enable properly the link interface betwen initial pages that is the home, login and the signup page.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import home from './pages/home'
+import login from './pages/login'
+import signup from './pages/signup'
+import Navbar from './components/Navbar'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar/>
+        <div className='container'>          
+          <Switch>
+            <Route exact path='/' component={home} />
+            <Route exact path='/login' component={login} />
+            <Route exact path='/signup' component={signup} />
+          </Switch>
+        </div>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Button from '@material-ui/core/Button'
+import Link from 'react-router-dom/Link'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <AppBar>
+                <Toolbar className='nav-container'>
+                    <Button color='inherit' component={Link} to='/'>Home</Button>
+                    <Button color='inherit' component={Link} to='/signup'>Signup</Button>
+                    <Button color='inherit' component={Link} to='/login'>Login</Button>
+                </Toolbar>
+            </AppBar>
+        )
+    }
+}
+
+export default Navbar
