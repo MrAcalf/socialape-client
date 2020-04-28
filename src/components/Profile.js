c82b53bf6355c85d3c3207148f268de6a1ccf9d6
Sandman <antonio.lima16@fatec.sp.gov.br>
2020-04-28 16:13:40 -0300

    Edit user details button and trasactions are ok and working on UI

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,17 +3,23 @@ import PropTypes from 'prop-types'
 import withStyles from '@material-ui/core/styles/withStyles'
 import { Link } from 'react-router-dom'
 import dayjs from 'dayjs'
+import EditDetails from './EditDetails'
 // MUI stuff
 import Button from '@material-ui/core/Button'
 import Paper from '@material-ui/core/Paper'
 import MuiLink from '@material-ui/core/Link'
 import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import Tooltip from '@material-ui/core/Tooltip'
 //Icons
 import LocationOnIcon from '@material-ui/icons/LocationOn'
 import LinkIcon from '@material-ui/icons/Link'
 import CalendarTodayIcon from '@material-ui/icons/CalendarToday'
+import EditIcon from '@material-ui/icons/Edit'
+import KeyboardReturnIcon from '@material-ui/icons/KeyboardReturn'
 //Redux
 import { connect } from 'react-redux'
+import { logoutUser, uploadImage } from '../redux/actions/userActions'
 
 const styles = (theme) => ({
     paper: {
@@ -64,6 +70,19 @@ const styles = (theme) => ({
 })
 
 class Profile extends Component {
+    handleImageChange = (event) => {
+        const image = event.target.files[0]
+        const formData = new FormData()
+        formData.append('image', image, image.name)
+        this.props.uploadImage(formData)
+    }
+    handleEditPicture = () => {
+        const fileInput = document.getElementById('imageInput')
+        fileInput.click()
+    }
+    handleLogout = () => {
+        this.props.logoutUser()
+    }
     render() {
         const { 
             classes, 
@@ -86,6 +105,17 @@ class Profile extends Component {
                 <div className={classes.profile}>
                     <div className='image-wrapper'>
                         <img src={imageUrl} alt='profile' className='profile-image' />
+                        <input 
+                        type='file' 
+                        id='imageInput'
+                        hidden='hidden' 
+                        onChange={this.handleImageChange}
+                        />
+                        <Tooltip title='Edit profile picture' placement='top'>
+                            <IconButton onClick={this.handleEditPicture} className='button'>
+                                <EditIcon color='primary'/>
+                            </IconButton>
+                        </Tooltip>
                     </div>
                     <hr/>
                     <div className='profile-details'>
@@ -94,11 +124,16 @@ class Profile extends Component {
                         to={`/users/${handle}`} 
                         color='primary' 
                         variant='h5'>
+                            
                             @{handle}
+                            
                         </MuiLink>
                         <hr/>
+
                         {bio && <Typography variant='body2'>{bio}</Typography>}
+
                         <hr/>
+                        
                         {location && (
                             <>
                                 <LocationOnIcon color='primary'/>
@@ -106,6 +141,7 @@ class Profile extends Component {
                                 <hr/>
                             </>
                         )}
+
                         {website && (
                             <>
                                 <LinkIcon color='primary'/>
@@ -115,8 +151,15 @@ class Profile extends Component {
                                 <hr/>
                             </>
                         )}
+
                         <CalendarTodayIcon color='primary'/>{' '}
                         <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
+                        <Tooltip title='Logout' placement='top'>
+                            <IconButton onClick={this.handleLogout}>
+                                <KeyboardReturnIcon color='primary'/>
+                            </IconButton>
+                        </Tooltip>
+                        <EditDetails/>
                     </div>
                 </div>
             </Paper>
@@ -144,9 +187,19 @@ const mapStateToProps = (state) => ({
     user: state.user
 })
 
+const mapActionsToProps = {
+    logoutUser,
+    uploadImage
+}
+
 Profile.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    uploadImage: PropTypes.func.isRequired,
     user: PropTypes.object.isRequired,
     classes: PropTypes.object.isRequired
 }
 
-export default connect(mapStateToProps )(withStyles(styles)(Profile))
+export default connect(
+    mapStateToProps,
+    mapActionsToProps
+)(withStyles(styles)(Profile))
--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -67,6 +67,24 @@ export const logoutUser = () => (dispatch) => {
     dispatch({ type: SET_UNAUTHENTICATED })
 }
 
+export const uploadImage = (formData) => (dispatch) => {
+    dispatch({ type: LOADING_USER })
+    axios.post('/user/image', formData)
+        .then(() => {
+            dispatch(getUserData())
+        })
+        .catch(err => console.log(err))
+}
+
+export const editUserDetails = (userDetails) => (dispatch) => {
+    dispatch({ type: LOADING_USER })
+    axios.post('/user', userDetails)
+        .then(() => {
+            dispatch(getUserData())
+        })
+        .catch(err => console.log(err))
+}
+
 const setAuthorizationHeader = (token) => {
     const FBIdToken = `Bearer ${token}`
     localStorage.setItem('FBIdToken', FBIdToken)
